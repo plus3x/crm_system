8cd1b6354f015a7035b550feb76e42b3a182207a
Vladislav Petrov <electronicchest@gmail.com>
2013-08-17 20:24:35 +0400

    Fix some problem with authorize

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+  
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user and user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect_to admin_url
+      session[:user_id] = user.id
+      redirect_to admin_url
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
