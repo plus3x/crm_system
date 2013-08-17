892589b7e5402ea3fc49a3eafaa2bde048c01e68
Vladislav Petrov <electronicchest@gmail.com>
2013-08-17 15:46:32 +0400

    Configure home page to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CrmSystem::Application.routes.draw do
   get "home/index"
+  root 'home#index', as: 'home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
