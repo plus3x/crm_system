fc0a2ea73408193d8b39b196007e8543aa977338
Vladislav Petrov <electronicchest@gmail.com>
2013-08-17 15:41:44 +0400

    Create project
    rails new crm_system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrmSystem::Application.config.secret_key_base = 'bc4b08006d0ab2f3fc3aa1fda5c627ba128730194f213903e101af32835f67caff6405a885b5595878808c4707e2f4a9b438306a650a457ac70ff83cc70e415e'
