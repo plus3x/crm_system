27a5b8cf66b930e06ca9cef2a7692eb9a68b3f80
Vladislav Petrov <electronicchest@gmail.com>
2013-08-20 04:07:14 +0400

    Fix coperighting bug - true redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
@@ -16,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Logged out"
+    redirect_to home_url, notice: "Logged out"
   end
 end
