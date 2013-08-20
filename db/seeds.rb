df6adde2ef8ae11f191f4bca9b7166851c7b9383
Vladislav Petrov <electronicchest@gmail.com>
2013-08-20 20:36:20 +0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Role.delete_all
+Role.create([ 
+  {id: 1, title: "admin"},
+  {id: 2, title: "client"}
+])
+puts "Стандартные роли:"
+puts Role.all.map(&:title)
+
+User.delete_all
+User.create([ 
+  {id: 1, email: "admin", password: "admin", password_confirmation: "admin", role_id: 1}
+])
+puts "Стандартные аккаунты:"
+puts User.all.map(&:email)
