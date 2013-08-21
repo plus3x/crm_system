73ff439ebc11ce2432d2f781e14c55de880f0f7e
Vladislav Petrov <electronicchest@gmail.com>
2013-08-21 22:36:55 +0400

    Generate Calendar
    rails generate event_calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CrmSystem::Application.routes.draw do
+  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
   scope '(:locale)' do
     resources :users
   
