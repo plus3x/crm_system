3454dd765b4abdc61713cd75b28157062257390c
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 04:20:47 +0400

    Delete test data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130821183539) do
+ActiveRecord::Schema.define(version: 20130817210601) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 20130821183539) do
     t.string   "inn"
     t.string   "ogrn"
     t.string   "actual_address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "events", force: true do |t|
-    t.string   "name"
-    t.datetime "start_at"
-    t.datetime "end_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
